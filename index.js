5c9cdc0ace4b310f0afa803e94f5643bb995c5c6
Walter Panacci <150435336+walterpanacci@users.noreply.github.com>
2024-03-14 14:53:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+//post to api/fileanalyse
 var app = express();
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,9 +20,11 @@ app.get('/', function (req, res) {
 });
 
 
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next){
+  const {originalname, mimetype, size} = req.file;
+  req.locals = [originalname, mimetype, size];
+  next()
+}, (req, res) => {
+  const [name, type, size] = req.locals;
+  res.json({name, type, size})
+})
